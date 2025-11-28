b14a6c12dbff19b61f20e2789aa6073087ee2cdc
Vong Hoang Vinh <vonghoangvinh1989@gmail.com>
2025-11-28 14:53:23 -0600

    added code related for frontend

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
-import './App.css'
-import Navigation from './components/Navigation/Navigation'
-import Logo from './components/Logo/Logo'
-import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm'
+import "./App.css";
+import Navigation from "./components/Navigation/Navigation";
+import Logo from "./components/Logo/Logo";
+import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
+import Rank from "./components/Rank/Rank";
+import ParticlesBg from "particles-bg";
 
 function App() {
   return (
     <div className="App">
-       <Navigation />
-       <Logo />
-       <ImageLinkForm />
-       {/* <FaceRecognition /> */}
+      <ParticlesBg className="particles" num={200} type="cobweb" bg={true} />
+      <Navigation />
+      <Logo />
+      <Rank />
+      <ImageLinkForm />
+      {/* <FaceRecognition /> */}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
